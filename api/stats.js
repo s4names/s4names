9d026076b8fd67ca56de3ce8c4183377f90dd3d5
s4n <r.ickyd.ick.yys@gmail.com>
2026-07-05 16:44:33 -0500

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,36 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 
-const DATA_FILE = path.join(__dirname, '..', 'data', 'users.json');
+const DATA_FILE = path.join(process.cwd(), 'data', 'users.json');
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
 
-    let users = {};
-    if (fs.existsSync(DATA_FILE)) {
-        users = JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
+    try {
+        let users = {};
+        if (fs.existsSync(DATA_FILE)) {
+            try {
+                users = JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
+            } catch (e) {
+                users = {};
+            }
+        }
+
+        const entries = Object.entries(users)
+            .filter(([_, data]) => data.verified)
+            .map(([id, data]) => ({
+                discordId: id,
+                ...(data.stats || { totalSnipes: 0, rareSnipes: 0, goodSnipes: 0, normalSnipes: 0, checkedCount: 0 })
+            }));
+
+        const totalSnipes = entries.reduce((s, u) => s + u.totalSnipes, 0);
+        const rareSnipes = entries.reduce((s, u) => s + u.rareSnipes, 0);
+        const goodSnipes = entries.reduce((s, u) => s + u.goodSnipes, 0);
+        const normalSnipes = entries.reduce((s, u) => s + u.normalSnipes, 0);
+        const checkedCount = entries.reduce((s, u) => s + u.checkedCount, 0);
+
+        const leaderboard = entries
+            .sort((a, b) => b.totalSnipes - a.totalSnipes)
+            .slice(0, 10);
+
+        return res.status(200).json({
+            totalSnipes,
+            rareSnipes,
+            goodSnipes,
+            normalSnipes,
+            checkedCount,
+            verifiedCount: entries.length,
+            leaderboard
+        });
+    } catch (e) {
+        return res.status(500).json({ error: 'Server error' });
     }
-
-    const userList = Object.entries(users).map(([id, data]) => ({
-        discordId: id,
-        ...data.stats
-    }));
-
-    const totalSnipes = userList.reduce((sum, u) => sum + (u.totalSnipes || 0), 0);
-    const rareSnipes = userList.reduce((sum, u) => sum + (u.rareSnipes || 0), 0);
-
-    const leaderboard = userList
-        .sort((a, b) => (b.totalSnipes || 0) - (a.totalSnipes || 0))
-        .slice(0, 10);
-
-    res.json({
-        totalSnipes,
-        rareSnipes,
-        goodSnipes: userList.reduce((sum, u) => sum + (u.goodSnipes || 0), 0),
-        normalSnipes: userList.reduce((sum, u) => sum + (u.normalSnipes || 0), 0),
-        checkedCount: userList.reduce((sum, u) => sum + (u.checkedCount || 0), 0),
-        verifiedCount: userList.length,
-        leaderboard
-    });
 };
